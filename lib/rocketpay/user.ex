b768ee520302d8c95a11c9806d148aa3bf11a241
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-02-25 00:10:50 -0300

    feat: add account schema and relation with user

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -2,6 +2,7 @@ defmodule Rocketpay.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Rocketpay.Account
   alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -15,6 +16,7 @@ defmodule Rocketpay.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+    has_one :account, Account
 
     timestamps()
   end

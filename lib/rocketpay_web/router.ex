9a96096992228a26ce31fa013daf0c85d1fd5e20
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-02-26 00:33:58 -0300

    feat: add account deposit controller

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -11,6 +11,10 @@ defmodule RocketpayWeb.Router do
     get "/:filename", WelcomeController, :index
 
     post "/users", UsersController, :create
+
+    post "/account/:id/deposit", AccountsController, :deposit
+
+    post "/account/:id/withdraw", AccountsController, :withdraw
   end
 
   # Enables LiveDashboard only for development

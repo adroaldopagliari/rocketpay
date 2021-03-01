1190739fb4be7105fc473571c30d9694965346a4
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-02-28 21:06:44 -0300

    test: ensure and view is generated when an user and account is created

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,28 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+    test "renders create.json" do
+      params = %{
+        name: "Adroaldo Pagliari",
+        password: "123456",
+        nickname: "adroaldo",
+        email: "adroaldo@outlook.com.br",
+        age: 32
+      }
+
+      {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} = Rocketpay.create_user(params)
+
+      response = render(UsersView, "create.json", user: user)
+
+      expected_response = %{message: "User created", user: %{account: %{balance: Decimal.new("0.00"),
+      id: account_id}, id: user_id,
+      name: "Adroaldo Pagliari", nickname: "adroaldo"}}
+
+      assert  expected_response == response
+    end
+  end
